7ed89d8d6f20b541ff9e51fc175d1637c7a795b8
ShaharYannay <shahar.yannay@experitest.com>
2019-06-19 15:15:51 +0300

    Wrong access key

--- 1/src/test/java/QuickTest.java
+++ 1/src/test/java/QuickTest.java
@@ -27,7 +27,7 @@ import java.net.URL;
 public class QuickTest {
 
 
-    private String accessKey = "eyJ4cC51IjoxMDEsInhwLnAiOjIsInhwLm0iOiJNVFV6TmpFek5EYzROVFE0T0EiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE4NTE0OTQ3ODUsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.g3wmvwwuO0NGOJwx-NhV7DLIfA3LOOcdwWa9p-xaFys";
+    private String accessKey = "eyJ4cC51IjoxMDEsInhwLnAiOjIsInhwLm0iOiJNVFV6TmpFek5EYzROVFE0T0EiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE4NTE0OTQ3ODUsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.g3wmvwwuO0NGOJwx-NhV7DLIfA3LOOcdwWa9p-xaFy";
     protected IOSDriver<IOSElement> iosDriver = null;
     protected AndroidDriver<AndroidElement> androidDriver = null;
 
